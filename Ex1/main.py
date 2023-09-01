174251916abf7083ac26262786991862fe821255
poweron0102 <nathanpinheiro99@gmail.com>
2023-09-01 17:05:11 -0300

    Janela

--- Ex1/main.py
+++ Ex1/main.py
@@ -1,5 +1,8 @@
-from PPlay import window
-from PPlay.window import Window
+from PPlay.window import *
 
-window = Window(200, 200)
-#uhuull
+window = Window(900, 600)
+window.set_title("Nathan Pinheiro")
+
+
+while True:
+    pass
