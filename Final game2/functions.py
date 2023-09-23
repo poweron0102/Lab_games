84db987784bf62c9bf9a43dd30791bc04c60dc16
poweron0102 <nathanpinheiro99@gmail.com>
2023-09-23 15:35:31 -0300

    cast walls with numba

--- Final game2/functions.py
+++ Final game2/functions.py
@@ -1,5 +1,8 @@
 import math
-def angle_to_fist(angle):
+from numba import njit
+
+@njit()
+def angle_to_fist(angle: float) -> float:
     if angle > 2 * math.pi:
         angle -= 2 * math.pi
     if angle < 0:
@@ -7,10 +10,12 @@ def angle_to_fist(angle):
     return angle
 
 
+@njit()
 def distancia(ponto_A, ponto_B):
     return math.sqrt(((ponto_A[0] - ponto_B[0]) ** 2) + (ponto_A[1] - ponto_B[1]) ** 2)
 
 
+@njit()
 def sig(numero):
     if numero < 0:
         return -1
--- Final game2/ray_caster.py
+++ Final game2/ray_caster.py
@@ -1,103 +1,117 @@
+from numba import njit
+
 from map import *
 from functions import *
+import numpy as np
 #from numba import njit
 import fast_ray_cast
 import math
+
+
+@njit()
+def cast_walls(player_x, player_y, player_ang, world_map, is_render=(1, 2, 3)):
+    # (0, (tamanho * math.cos(player_ang - angle_ray), contador, ponto, look))
+    # rays = np.empty(RES[0] // SCALE, dtype=np.dtype( [np.uint8, [float, np.uint16, [float, float], np.uint8]] ))
+    # rays = [None] * (RES[0] // SCALE)
+    rays = []
+    offset_x = offset_y = 0
+    angle_ray = angle_to_fist(player_ang - np.deg2rad(FOV) / 2)
+
+    for contador in range(RES[0] // SCALE):
+        # Calculo horizontal -=-=-=-=-=-=-=--=-=-=-=-
+        rendist = 0
+        a_tan = -1 / (np.tan(angle_ray) + 0.00001)
+        if angle_ray > math.pi:  # looking up
+            ray_y = (player_y // Tile_size) * Tile_size - 0.0001
+            ray_x = player_x + ((player_y - ray_y) * a_tan)
+            offset_y = -Tile_size
+            offset_x = -offset_y * a_tan
+            look_ns = 1
+        elif angle_ray < math.pi:  # looking down
+            ray_y = (player_y // Tile_size) * Tile_size + Tile_size
+            ray_x = player_x + ((player_y - ray_y) * a_tan)
+            offset_y = Tile_size
+            offset_x = -offset_y * a_tan
+            look_ns = 0
+        else:  # if angle_ray == 0 or angle_ray == math.pi:
+            ray_y = player_y
+            ray_x = player_x
+            rendist = Render_dist
+
+        while rendist < Render_dist:
+            y = int(ray_y // Tile_size)
+            if 0 <= y < len(world_map):
+                x = int(ray_x // Tile_size)
+                if 0 <= x < len(world_map[y]):
+                    if world_map[y][x] in is_render:
+                        break
+            # else:
+            ray_x += offset_x
+            ray_y += offset_y
+            rendist += 1
+
+        ray_posi_h = (ray_x, ray_y)
+
+        # Calculo vertical -=-=-=-=-=-=-=-=-=-=
+        rendist = 0
+        a_tan = -math.tan(angle_ray)
+        if angle_ray < math.pi / 2 or angle_ray > math.pi * 3 / 2:  # looking right
+            ray_x = (player_x // Tile_size) * Tile_size + Tile_size
+            ray_y = player_y + ((player_x - ray_x) * a_tan)
+            offset_x = Tile_size
+            offset_y = -offset_x * a_tan
+            look_ew = 3
+        elif math.pi / 2 < angle_ray < math.pi * 3 / 2:  # looking left
+            ray_x = (player_x // Tile_size) * Tile_size - 0.0001
+            ray_y = player_y + ((player_x - ray_x) * a_tan)
+            offset_x = -Tile_size
+            offset_y = -offset_x * a_tan
+            look_ew = 2
+        else:
+            ray_y = player_y
+            ray_x = player_x
+            rendist = Render_dist
+
+        while rendist < Render_dist:
+            y = int(ray_y // Tile_size)
+            if 0 <= y < len(world_map):
+                x = int(ray_x // Tile_size)
+                if 0 <= x < len(world_map[y]):
+                    if world_map[y][x] in is_render:
+                        break
+            # else:
+            ray_x += offset_x
+            ray_y += offset_y
+            rendist += 1
+
+        ray_posi_v = (ray_x, ray_y)
+
+        dist_h = distancia((player_x, player_y), ray_posi_h)
+        dist_v = distancia((player_x, player_y), ray_posi_v)
+
+        if dist_h < dist_v:
+            ponto = ray_posi_h
+            tamanho = dist_h
+            look = look_ns
+        else:
+            ponto = ray_posi_v
+            tamanho = dist_v
+            look = look_ew
+
+        # rays[contador] = (0, (tamanho * math.cos(player_ang - angle_ray), contador, ponto, look))
+        rays.append((0, (tamanho * math.cos(player_ang - angle_ray), contador, ponto, look)))
+
+        angle_ray += math.radians(FOV) / (RES[0] // SCALE)
+        angle_ray = angle_to_fist(angle_ray)
+
+    return rays
 
 
 class RayCaster:
     def __init__(self, game):
         self.game = game
 
-    #@njit()
-    def ray_size(self):
-        player_x = self.game.player.x
-        player_y = self.game.player.y
-        player_ang = self.game.player.ang
-        offset_x = offset_y = 0
-        angle_ray = angle_to_fist(player_ang - math.radians(FOV) / 2)
-
-        for contador in range(RES[0] // SCALE):
-            # Calculo horizontal -=-=-=-=-=-=-=--=-=-=-=-
-            rendist = 0
-            a_tan = -1 / (math.tan(angle_ray) + 0.00001)
-            if angle_ray > math.pi:  # looking up
-                ray_y = (player_y // Tile_size) * Tile_size - 0.0001
-                ray_x = player_x + ((player_y - ray_y) * a_tan)
-                offset_y = -Tile_size
-                offset_x = -offset_y * a_tan
-                look_ns = 1
-            elif angle_ray < math.pi:  # looking down
-                ray_y = (player_y // Tile_size) * Tile_size + Tile_size
-                ray_x = player_x + ((player_y - ray_y) * a_tan)
-                offset_y = Tile_size
-                offset_x = -offset_y * a_tan
-                look_ns = 0
-            else:  # if angle_ray == 0 or angle_ray == math.pi:
-                ray_y = player_y
-                ray_x = player_x
-                rendist = Render_dist
-
-            while rendist < Render_dist:
-                if Tile_size < ray_x < RES[0] + Tile_size and -Tile_size < ray_y < RES[1] + Tile_size:
-                    if self.game.map.is_render(ray_x, ray_y):
-                        break
-                # else:
-                ray_x += offset_x
-                ray_y += offset_y
-                rendist += 1
-
-            ray_posi_h = (ray_x, ray_y)
-
-            # Calculo vertical -=-=-=-=-=-=-=-=-=-=
-            rendist = 0
-            a_tan = -math.tan(angle_ray)
-            if angle_ray < math.pi / 2 or angle_ray > math.pi * 3 / 2:  # looking right
-                ray_x = (player_x // Tile_size) * Tile_size + Tile_size
-                ray_y = player_y + ((player_x - ray_x) * a_tan)
-                offset_x = Tile_size
-                offset_y = -offset_x * a_tan
-                look_ew = 3
-            elif math.pi / 2 < angle_ray < math.pi * 3 / 2:  # looking left
-                ray_x = (player_x // Tile_size) * Tile_size - 0.0001
-                ray_y = player_y + ((player_x - ray_x) * a_tan)
-                offset_x = -Tile_size
-                offset_y = -offset_x * a_tan
-                look_ew = 2
-            else:
-                ray_y = player_y
-                ray_x = player_x
-                rendist = Render_dist
-
-            while rendist < Render_dist:
-                if Tile_size < ray_x < RES[0] + Tile_size and -Tile_size < ray_y < RES[1] + Tile_size:
-                    if self.game.map.is_render(ray_x, ray_y):
-                        break
-                # else:
-                ray_x += offset_x
-                ray_y += offset_y
-                rendist += 1
-
-            ray_posi_v = (ray_x, ray_y)
-
-            dist_h = distancia((player_x, player_y), ray_posi_h)
-            dist_v = distancia((player_x, player_y), ray_posi_v)
-
-            if dist_h < dist_v:
-                ponto = ray_posi_h
-                tamanho = dist_h
-                look = look_ns
-            else:
-                ponto = ray_posi_v
-                tamanho = dist_v
-                look = look_ew
-
-            self.game.drawer.to_draw.append((0, (tamanho * math.cos(self.game.player.ang - angle_ray), contador, ponto, look)))
-
-            angle_ray += math.radians(FOV) / (RES[0] // SCALE)
-            angle_ray = angle_to_fist(angle_ray)
-
-    def ray_size_fast(self):
+    def ray_size_rust(self):
         self.game.drawer.to_draw.extend(fast_ray_cast.cast(
             NumThreads,
             self.game.player.x,
@@ -113,11 +127,19 @@ class RayCaster:
         ))
         # print(self.rays)
 
+    def ray_size_python(self):
+        self.game.drawer.to_draw.extend(cast_walls(
+            self.game.player.x,
+            self.game.player.y,
+            self.game.player.ang,
+            self.game.map.world_map,
+        ))
+
     def update(self):
         if RUST:
-            self.ray_size_fast()
+            self.ray_size_rust()
         else:
-            self.ray_size()
+            self.ray_size_python()
 
     #def draw_rays(self, screen):
     #    self.ray_size()
